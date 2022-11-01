c65cfb4bbb2274cbda4e13ddf6f1566ab39dc66b
devch96 <chjung1006@gmail.com>
2022-11-02 01:33:45 +0900

    두 수의 차

--- pro/lv0/minus.java
+++ pro/lv0/minus.java
@@ -1,6 +1,8 @@
-class Solution {
-    public int solution(int num1, int num2) {
-        int answer = num1 - num2 ;
-        return answer;
+public class minus{
+    class Solution {
+        public int solution(int num1, int num2) {
+            int answer = num1 - num2 ;
+            return answer;
+        }
     }
 }
