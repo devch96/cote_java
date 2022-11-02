53a47a5e990d0cb1b426e2f3b5120b8fde2e6e73
devch96 <chjung1006@gmail.com>
2022-11-03 02:51:19 +0900

    배열 원소의 길이

---
+++ pro/lv0/LengthArray.java
@@ -0,0 +1,9 @@
+public class LengthArray {
+    public int[] solution(String[] strlist){
+        int[] answer = new int[strlist.length];
+        for (int i = 0; i < strlist.length; i++) {
+            answer[i] = strlist[i].length();
+        }
+        return answer;
+    }
+}
