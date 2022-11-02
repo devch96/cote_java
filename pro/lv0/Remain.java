b6daee6e740c532b785d87ecd54f804f49024de2
devch96 <chjung1006@gmail.com>
2022-11-02 19:20:49 +0900

    나머지 구하기

--- pro/lv0/Remain.java
+++ pro/lv0/Remain.java
@@ -1,3 +1,8 @@
 public class Remain {
-    class Solution
+    class Solution{
+        public int solution(int num1, int num2) {
+            int answer = num1 % num2;
+            return answer;
+        }
+    }
 }
