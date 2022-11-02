27531046eaca53bf2e6ef107b6c6a003d5154919
devch96 <chjung1006@gmail.com>
2022-11-02 19:27:25 +0900

    숫자 비교하기

---
+++ pro/lv0/Compare.java
@@ -0,0 +1,12 @@
+public class Compare {
+    public int solution(int num1, int num2){
+        if(num1 == num2){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
+
+//    int answer = (num1 == num2) ? 1: -1;
+//    return answer;
+}
