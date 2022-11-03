3dc9a6a6359c2b200cbabcea7d889e1dd03d4a3f
devch96 <chjung1006@gmail.com>
2022-11-04 02:19:48 +0900

    특정 문자 제거하기

---
+++ pro/lv0/ReplaceString.java
@@ -0,0 +1,16 @@
+public class ReplaceString {
+    public String solution(String my_string, String letter){
+        my_string = my_string.replace(letter, "");
+        return my_string;
+//        String answer = "";
+//        char c = letter.charAt(0);
+//        for (int i = 0; i < my_string.length(); i++) {
+//            char temp = my_string.charAt(i);
+//            if (temp == c) {
+//                continue;
+//            }
+//            answer += temp;
+//        }
+//        return answer;
+    }
+}
