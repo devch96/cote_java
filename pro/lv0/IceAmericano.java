565c6a846f40ec02a7ee8667b557663a56ebd426
devch96 <chjung1006@gmail.com>
2022-11-03 11:57:58 +0900

    문자열 뒤집기

---
+++ pro/lv0/IceAmericano.java
@@ -0,0 +1,9 @@
+public class IceAmericano {
+    public int[] solution(int money) {
+        int[] answer = new int[2];
+        answer[0] = money / 5500;
+        answer[1] = money % 5500;
+        return answer;
+//        return new int[] {money / 5500, money % 5500};
+    }
+}
