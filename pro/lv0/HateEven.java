a73f985227aca071949692084dd420f43f9a610e
devch96 <chjung1006@gmail.com>
2022-11-06 11:50:04 +0900

    짝수는 싫어요

---
+++ pro/lv0/HateEven.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HateEven {
+    public int[] solution(int n) {
+        int[] answer = new int[(n+1)/2];
+        int a = 0;
+        for(int i = 1; i<n+1; i++){
+            if(i%2!=0){
+                answer[a] = i;
+                a++;
+            }
+        }
+//        List<Integer> answer = new ArrayList<>();
+//        for (int i = 1; i <= n; i++) {
+//            if (i % 2 == 1) {
+//                answer.add(i);
+//            }
+//        }
+//        return answer.stream().mapToInt(x -> x).toArray();
+        return answer;
+    }
+}
