d55b15ff0ca43cccb43afe1ceef44bc286c63e9a
devch96 <chjung1006@gmail.com>
2022-11-07 14:16:56 +0900

    숨어있는 숫자의 덧셈 (1)

---
+++ pro/lv0/AddHiddenNum_1.java
@@ -0,0 +1,11 @@
+public class AddHiddenNum_1 {
+    public int solution(String my_string){
+        int answer = 0;
+        for(char ch : my_string.toCharArray()){
+            if (Character.isDigit(ch)){
+                answer += (int) (ch-'0');
+            }
+        }
+        return answer;
+    }
+}
