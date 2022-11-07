c32290b621be13dace67ce26cb9ba32e562bd2f9
devch96 <chjung1006@gmail.com>
2022-11-07 14:42:05 +0900

    약수 구하기

---
+++ pro/lv0/FindMeasure.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class FindMeasure {
+    public List<Integer> solution(int n){
+        List<Integer> answer = new ArrayList<>();
+        for(int i = 1 ; i <= (int) Math.sqrt(n); i++){
+            if(n % i == 0){
+                answer.add(i);
+                if(n/i != i){
+                    answer.add(n/i);
+                }
+            }
+        }
+        Collections.sort(answer);
+        return answer;
+    }
+}
