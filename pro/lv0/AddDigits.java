dbd5066f4a99f57e4dfc802a37cacc6c93a5c6ce
devch96 <chjung1006@gmail.com>
2022-11-07 14:03:20 +0900

    자릿수 더하기

---
+++ pro/lv0/AddDigits.java
@@ -0,0 +1,19 @@
+public class AddDigits {
+    public int solution(int n) {
+        int answer = 0;
+        String str = String.valueOf(n);
+        for(char str1 : str.toCharArray()){
+            answer += (int)(str1 - '0');
+        }
+        return answer;
+//        while(n>0)
+//
+//        {
+//            answer += n % 10;
+//            n /= 10;
+//        }
+//        return answer;
+    }
+
+
+}
