bbab78ef9537624c4132aa9c87fac2ee1f55fa6a
devch96 <chjung1006@gmail.com>
2022-11-08 18:34:35 +0900

    주사위의 개수

---
+++ pro/lv0/NumberOfDice.java
@@ -0,0 +1,9 @@
+public class NumberOfDice {
+    public int solution(int[] box, int n){
+        int answer = 1;
+        for(int i : box){
+            answer *= i/n;
+        }
+        return answer;
+    }
+}
