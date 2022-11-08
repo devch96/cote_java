5f0060e5dbc58d076e5b4f13929c8c5ca206f13a
devch96 <chjung1006@gmail.com>
2022-11-08 18:32:28 +0900

    가위 바위 보

---
+++ pro/lv0/RockPaperScissors.java
@@ -0,0 +1,13 @@
+public class RockPaperScissors {
+    public String solution(String rsp){
+        String answer = "";
+        for(char ch : rsp.toCharArray()){
+            if(ch == '2'){
+                answer += '0';
+            } else if (ch == '5') {
+                answer += '2';
+            }else answer += '5';
+        }
+        return answer;
+    }
+}
