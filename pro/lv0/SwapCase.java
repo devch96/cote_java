080d8acf35a9f48c4ff6bfb39e944de80cbbf6eb
devch96 <chjung1006@gmail.com>
2022-11-08 18:24:43 +0900

    대문자와 소문자

---
+++ pro/lv0/SwapCase.java
@@ -0,0 +1,13 @@
+public class SwapCase {
+    public String solution(String my_string){
+        String answer = "";
+        for(char ch : my_string.toCharArray()){
+            if(Character.isUpperCase(ch)){
+                answer += Character.toLowerCase(ch);
+            }else {
+                answer += Character.toUpperCase(ch);
+            }
+        }
+        return answer;
+    }
+}
