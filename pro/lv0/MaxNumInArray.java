44292730675e32dcc87aeecad944fd89fc562b24
devch96 <chjung1006@gmail.com>
2022-11-08 18:15:35 +0900

    가장 큰 수 찾기

---
+++ pro/lv0/MaxNumInArray.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class MaxNumInArray {
+    public int[] solution(int[] array){
+        int max = array[0];
+        int index = 0;
+        for(int i=0; i< array.length; i++){
+            if(max<array[i]){
+                max = array[i];
+                index = i;
+            }
+        }
+        int[]answer = new int[]{max, index};
+        return answer;
+    }
+}
