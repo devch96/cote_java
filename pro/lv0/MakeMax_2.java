100cdf8a55119fb1f5a8fd68c014ff13edfb86a2
devch96 <chjung1006@gmail.com>
2022-11-10 17:05:08 +0900

    최댓값 만들기 (2)

---
+++ pro/lv0/MakeMax_2.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class MakeMax_2 {
+    public int solution(int[] numbers){
+        Arrays.sort(numbers);
+        return numbers[0] * numbers[1] > numbers[numbers.length] * numbers[numbers.length-1] ?
+                numbers[0] * numbers[1] : numbers[numbers.length] * numbers[numbers.length-1];
+    }
+}
