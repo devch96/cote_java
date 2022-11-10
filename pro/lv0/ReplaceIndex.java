7658d701543b3344d48615d73664d68ea1fbd192
devch96 <chjung1006@gmail.com>
2022-11-10 16:59:12 +0900

    인덱스 바꾸기

---
+++ pro/lv0/ReplaceIndex.java
@@ -0,0 +1,10 @@
+public class ReplaceIndex {
+    public String solution(String my_string, int num1, int num2){
+        char idx1 = my_string.charAt(num1);
+        char idx2 = my_string.charAt(num2);
+        StringBuilder answer = new StringBuilder(my_string);
+        answer.setCharAt(num1, idx2);
+        answer.setCharAt(num2, idx1);
+        return answer.toString();
+    }
+}
