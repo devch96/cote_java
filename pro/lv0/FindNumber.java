83649c8c2136690c1133b9f4a51ed1a6196b3048
devch96 <chjung1006@gmail.com>
2022-11-10 16:54:40 +0900

    숫자 찾기

---
+++ pro/lv0/FindNumber.java
@@ -0,0 +1,8 @@
+public class FindNumber {
+    public int solution(int num, int k){
+        int answer = Integer.toString(num).indexOf(Integer.toString(k));
+        if(answer==-1){
+            return -1;
+        }else return answer+1;
+    }
+}
