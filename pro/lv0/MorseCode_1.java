7a50e1de8d3a0579f70105a3a8fc6bcfad7f3a07
devch96 <chjung1006@gmail.com>
2022-11-14 16:26:07 +0900

    모스부호 (1)

---
+++ pro/lv0/MorseCode_1.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class MorseCode_1 {
+    public String solution(String letter){
+        String answer = "";
+        String[] morse = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        String[] letterArray = letter.split(" ");
+        HashMap<String, Character> hashMap = new HashMap<String, Character>();
+        for(int i = 0 ; i < morse.length; i++){
+            hashMap.put(morse[i], (char)(97 + i));
+        }
+        for(int i = 0; i < letterArray.length; i++){
+            answer += hashMap.get(letterArray[i]);
+        }
+        return answer;
+    }
+}
