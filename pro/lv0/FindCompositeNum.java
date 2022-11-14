7c65fb88176b19e06368a724ffeab4f2bcb977cd
devch96 <chjung1006@gmail.com>
2022-11-14 14:26:48 +0900

    합성수 찾기

---
+++ pro/lv0/FindCompositeNum.java
@@ -0,0 +1,25 @@
+public class FindCompositeNum {
+    public int solution(int n){
+        int primeNum = 0;
+        for(int i = 1; i <= n ; i ++){
+            if(isPrime(i)){
+                primeNum++;
+            }
+        }
+        return n-primeNum-1;
+    }
+    public boolean isPrime(int n){
+        if(n == 1){
+            return false;
+        }
+        if(n == 2){
+            return true;
+        }
+        for(int i = 2; i<=((int)(Math.sqrt(n))); i++){
+            if(n%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
