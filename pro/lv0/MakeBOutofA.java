9b200e65e1fc83dbd8ff650484286ebb541af268
devch96 <chjung1006@gmail.com>
2022-11-18 16:31:26 +0900

    A로 B 만들기

---
+++ pro/lv0/MakeBOutofA.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class MakeBOutofA {
+    public int solution(String before, String after){
+        char[] charsB = before.toCharArray();
+        char[] charsA = after.toCharArray();
+        Arrays.sort(charsB);
+        Arrays.sort(charsA);
+        String sortedB = new String(charsB);
+        String sortedA = new String(charsA);
+        if(sortedB.equals(sortedA)){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+}
