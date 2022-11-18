ab49e0d331f33ec57b35a447d03deeeab1b514f9
devch96 <chjung1006@gmail.com>
2022-11-18 16:54:40 +0900

    k의 개수

---
+++ pro/lv0/CountK.java
@@ -0,0 +1,9 @@
+public class CountK {
+    public int solution(int i, int j, int k){
+        int answer = 0;
+        for(int x = i; x<=j ; x++){
+            answer += String.valueOf(x).chars().filter(c -> c == (char) (k + '0')).count();
+        }
+        return answer;
+    }
+}
