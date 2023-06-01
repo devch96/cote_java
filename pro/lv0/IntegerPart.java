5cab200122f87b788a49c98a602b603b446f47ae
devch96 <chjung1006@gmail.com>
2023-06-01 19:07:13 +0900

    정수 부분

---
+++ pro/lv0/IntegerPart.java
@@ -0,0 +1,13 @@
+public class IntegerPart {
+    static class Solution{
+        public int solution(double flo){
+            int answer = (int) flo;
+            return answer;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(1.3));
+    }
+}
