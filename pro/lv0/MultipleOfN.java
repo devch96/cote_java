6fd5137d8db34136a313fa96ab298085ac52ca91
devch96 <chjung1006@gmail.com>
2023-06-01 19:13:33 +0900

    n의 배수

---
+++ pro/lv0/MultipleOfN.java
@@ -0,0 +1,16 @@
+public class MultipleOfN {
+    static class Solution{
+        public int solution(int num, int n){
+            if(num % n == 0){
+                return 1;
+            }else{
+                return 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(98,2));
+    }
+}
