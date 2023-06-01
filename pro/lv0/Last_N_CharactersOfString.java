bbfd35a2a64ff62680ca0d15ebda9eb2e452a870
devch96 <chjung1006@gmail.com>
2023-06-01 19:17:56 +0900

    문자열의 뒤의 n글자

---
+++ pro/lv0/Last_N_CharactersOfString.java
@@ -0,0 +1,11 @@
+public class Last_N_CharactersOfString {
+
+    public String solution(String my_string, int n){
+        return my_string.substring(my_string.length()-n);
+    }
+
+    public static void main(String[] args) {
+        Last_N_CharactersOfString l = new Last_N_CharactersOfString();
+        System.out.println(l.solution("ProgrammerS123",11));
+    }
+}
