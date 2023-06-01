8c6195e540ad44cbde1cfa295fe91c1ca93fc273
devch96 <chjung1006@gmail.com>
2023-06-01 19:20:11 +0900

    문자열로 변환

---
+++ pro/lv0/ConvertToString.java
@@ -0,0 +1,10 @@
+public class ConvertToString {
+    public String solution(int n){
+        return Integer.toString(n);
+    }
+
+    public static void main(String[] args) {
+        ConvertToString convertToString = new ConvertToString();
+        System.out.println(convertToString.solution(123));
+    }
+}
