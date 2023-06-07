249195c9de994425a44cb428032a98a8a6fcd2fa
devch96 <chjung1006@gmail.com>
2023-06-07 11:40:31 +0900

    공배수

---
+++ pro/lv0/CommonMultiple.java
@@ -0,0 +1,9 @@
+public class CommonMultiple {
+    public static int solution(int number, int n, int m){
+        return number % n == 0 && number % m == 0 ?  1 :  0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(CommonMultiple.solution(55,10,5));
+    }
+}
