4cade6f6ef8ecf12a752cef008297b52a691d7c6
devch96 <chjung1006@gmail.com>
2023-06-09 18:33:28 +0900

    원소들의 곱과 합

---
+++ pro/lv0/MulAndSumOfElements.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class MulAndSumOfElements {
+    public static int solution(int[] num_list){
+        return Arrays.stream(num_list).reduce(1, (a,b) -> a*b) < Math.pow(Arrays.stream(num_list).sum(),2)? 1 : 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(MulAndSumOfElements.solution(new int[] {3,4,5,2,1}));
+    }
+}
