acf25f805426ab7717eb0c875af650242db47891
devch96 <chjung1006@gmail.com>
2023-06-09 18:10:29 +0900

    문자열 정수의 합

---
+++ pro/lv0/SumOfStringIntegers.java
@@ -0,0 +1,10 @@
+public class SumOfStringIntegers {
+    public static int solution(String num_str){
+//        return num_str.chars().map(c -> c-48).sum();
+        return num_str.chars().reduce(0, (acc,c) -> c - '0' + acc);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(SumOfStringIntegers.solution("123456789"));
+    }
+}
