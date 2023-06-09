f2645b9bff6a334f45341b570ebb413ec9e15fba
devch96 <chjung1006@gmail.com>
2023-06-09 18:13:11 +0900

    부분 문자열인지 확인하기

---
+++ pro/lv0/CheckSubstring.java
@@ -0,0 +1,9 @@
+public class CheckSubstring {
+    public static int solution(String my_string, String target){
+        return my_string.contains(target) ? 1 : 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(CheckSubstring.solution("banana","wxyz"));
+    }
+}
