86fc0ad9deca561e85161ca8bd7d18d88d06b3e1
devch96 <chjung1006@gmail.com>
2023-06-09 18:24:07 +0900

    조건에 맞게 수열 변환하기 3

--- pro/lv0/ConvertSequence_3.java
+++ pro/lv0/ConvertSequence_3.java
@@ -1,7 +1,11 @@
 import java.util.Arrays;
 
 public class ConvertSequence_3 {
-    public int[] solution(int[] arr, int k){
+    public static int[] solution(int[] arr, int k){
         return Arrays.stream(arr).map(k%2 == 0 ? i -> i+k : i -> i*k).toArray();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(ConvertSequence_3.solution(new int[] {1,2,3,100,99,98},3)));
     }
 }
