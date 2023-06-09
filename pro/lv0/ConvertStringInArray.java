9382c9715dedc64fa6d772b33fb97b39d724dc90
devch96 <chjung1006@gmail.com>
2023-06-09 19:00:38 +0900

    배열에서 문자열 대소문자 변환하기

---
+++ pro/lv0/ConvertStringInArray.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class ConvertStringInArray {
+    public static String[] solution(String[] strArr){
+        return IntStream.range(0, strArr.length)
+                .mapToObj(i -> i % 2 == 0 ? strArr[i].toLowerCase() : strArr[i].toUpperCase())
+                .toArray(String[]::new);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(ConvertStringInArray.solution(new String[] {"AAA","BBB","CCC","DDD"})));
+    }
+}
