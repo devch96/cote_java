2cbc96dc1dd5b6659209356be31a6cab51b421f8
devch96 <chjung1006@gmail.com>
2023-06-14 10:20:20 +0900

    n보다 커질 때까지 더하기

---
+++ pro/lv0/AddUntilGreaterThanN.java
@@ -0,0 +1,12 @@
+public class AddUntilGreaterThanN {
+    public static int solution(int[] numbers, int n){
+        int answer = 0;
+        for(int i : numbers){
+            answer += i;
+            if(answer > n){
+                return answer;
+            }
+        }
+        return answer;
+    }
+}
