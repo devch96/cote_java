db8921d4aa92dc7b8ba25a4cb1969ad96c0ea894
devch96 <chjung1006@gmail.com>
2023-06-15 20:00:24 +0900

    이어 붙인 수

---
+++ pro/lv0/NumberConcat.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class NumberConcat {
+    public static int solution(int[] num_list){
+        StringBuilder even = new StringBuilder();
+        StringBuilder odd = new StringBuilder();
+        Arrays.stream(num_list).forEach(n -> {
+            if(n % 2 == 0){
+                even.append(n);
+            }else{
+                odd.append(n);
+            }
+        });
+        return Integer.parseInt(even.toString()) + Integer.parseInt(odd.toString());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[] {3, 4, 5, 2, 1}));
+    }
+}
