e5e2feca725ce0c9049661b8307a90382a3a2a76
devch96 <chjung1006@gmail.com>
2023-06-15 19:41:05 +0900

    원하는 문자열 찾기

---
+++ pro/lv0/FindString.java
@@ -0,0 +1,9 @@
+public class FindString {
+    public static int solution(String myString, String pat){
+        return myString.toLowerCase().contains(pat.toLowerCase()) ? 1 : 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("AbCdEfG","aBc"));
+    }
+}
