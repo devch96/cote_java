4e365f3011c648fe004a8200cd00e97a18038055
devch96 <chjung1006@gmail.com>
2023-06-18 18:10:49 +0900

    배열 비교하기

---
+++ pro/lv0/CompareArray_2.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class CompareArray_2 {
+    public static int solution(int[] arr1, int[] arr2) {
+        if(arr1.length != arr2.length){
+            return arr1.length > arr2.length ? 1 : -1;
+        }else {
+            if(Arrays.stream(arr1).sum() == Arrays.stream(arr2).sum()){
+                return 0;
+            }
+            return Arrays.stream(arr1).sum() > Arrays.stream(arr2).sum() ? 1 : -1;
+        }
+    }
+}
