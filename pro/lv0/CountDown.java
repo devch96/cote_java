0483366ce8c535b428c8d6596ffa56cc531cc8df
devch96 <chjung1006@gmail.com>
2023-06-18 17:10:06 +0900

    카운트 다운

--- pro/lv0/CountDown.java
+++ pro/lv0/CountDown.java
@@ -1,9 +1,11 @@
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.stream.IntStream;
 
 public class CountDown {
     public static int[] solution(int start, int end){
-        return Arrays.stream(IntStream.rangeClosed(start,end).toArray()).toArray();
+        return Arrays.stream(IntStream.rangeClosed(end,start).boxed().sorted(Comparator.reverseOrder()).mapToInt(Integer::intValue).toArray()).toArray();
+
     }
 
     public static void main(String[] args) {
