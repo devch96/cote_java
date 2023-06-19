9e5ca1df4f689d4d5d3c77ad8825d70cdc9f5c81
devch96 <chjung1006@gmail.com>
2023-06-19 17:12:04 +0900

    배열의 원소 삭제하기

---
+++ pro/lv0/DeleteElement.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class DeleteElement {
+    public static int[] solution(int[] arr, int[] delete_list){
+        return Arrays.stream(arr).filter(i -> !Arrays.asList(Arrays.stream(delete_list).boxed().toArray(Integer[]::new)).contains(i)).toArray();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[] {293, 1000, 395, 678, 94}, new int[] {94, 777, 104, 1000, 1, 12})));
+    }
+}
