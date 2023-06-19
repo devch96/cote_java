16cd9d7d8ddf23079d9790637ad45d95adab7fee
devch96 <chjung1006@gmail.com>
2023-06-19 18:49:19 +0900

    특별한 이차원 배열 1

---
+++ pro/lv0/Special2DArr_1.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Special2DArr_1 {
+    public static int[][] solution(int n){
+        int[][] answer = new int[n][n];
+        for(int i = 0; i < n; i++){
+               answer[i][i] = 1;
+            }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        for(int[] arr : solution(3)){
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+}
