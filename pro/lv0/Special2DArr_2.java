436e5d6abc8586ae2c0a0c90fb30d2f73f05f553
devch96 <chjung1006@gmail.com>
2023-06-19 18:36:48 +0900

    특별한 이차원 배열 2

---
+++ pro/lv0/Special2DArr_2.java
@@ -0,0 +1,17 @@
+import java.util.stream.IntStream;
+
+public class Special2DArr_2 {
+    public static int solution(int[][] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                if(arr[i][j] != arr[j][i]){
+                    return 0;
+                }
+            }
+        }
+        return 1;
+
+//        return IntStream.range(0, arr.length).allMatch(i -> IntStream.range(i + 1, arr.length).allMatch(j -> arr[i][j] == arr[j][i])) ? 1 : 0;
+
+    }
+}
