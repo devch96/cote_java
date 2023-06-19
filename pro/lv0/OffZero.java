b29840a4a2ed14c51520f5083bd9131ac8334441
devch96 <chjung1006@gmail.com>
2023-06-19 16:19:37 +0900

    0 떼기

---
+++ pro/lv0/OffZero.java
@@ -0,0 +1,9 @@
+public class OffZero {
+    public static String solution(String n_str){
+        return ""+Integer.parseInt(n_str);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("0010"));
+    }
+}
