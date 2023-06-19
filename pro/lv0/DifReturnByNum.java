6bc96cfc15ed158968ad86dea06167bf14aa206f
devch96 <chjung1006@gmail.com>
2023-06-19 16:14:55 +0900

    홀짝에 따라 다른 값 반환하기

---
+++ pro/lv0/DifReturnByNum.java
@@ -0,0 +1,15 @@
+import java.util.stream.IntStream;
+
+public class DifReturnByNum {
+    public static int solution(int n) {
+        if(n%2 == 0){
+            return IntStream.rangeClosed(1,n).filter(i -> i%2 == 0).reduce(0, (acc,operand) -> acc+(operand*operand));
+        } else{
+            return IntStream.rangeClosed(1,n).filter(i -> i%2 != 0).reduce(0, (acc,operand) -> acc + operand);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(10));
+    }
+}
