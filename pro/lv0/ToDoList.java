5e6e158a54c2654523f4210ba6cc2a1596095fba
devch96 <chjung1006@gmail.com>
2023-06-19 16:58:12 +0900

    할 일 목록

---
+++ pro/lv0/ToDoList.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class ToDoList {
+    public static String[] solution(String[] todo_list, boolean[] finished){
+        return IntStream.range(0, finished.length).filter(i -> !finished[i]).mapToObj(i -> todo_list[i]).toArray(String[]::new);
+    }
+}
