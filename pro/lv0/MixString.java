c73bbe8778ba6067c0335323a0a6bfc2e40dcf2f
devch96 <chjung1006@gmail.com>
2023-06-20 18:50:00 +0900

    문자열 섞기

---
+++ pro/lv0/MixString.java
@@ -0,0 +1,11 @@
+import java.util.stream.IntStream;
+
+public class MixString {
+    public static String solution(String str1, String str2){
+        return String.join("",IntStream.range(0,str1.length()).mapToObj(i -> String.valueOf(str1.charAt(i)).concat(String.valueOf(str2.charAt(i)))).toArray(String[]::new));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("aaaaa","bbbbb"));
+    }
+}
