3e5d861753b2c72e82ba85633ed732b9da175366
devch96 <chjung1006@gmail.com>
2023-06-20 19:40:50 +0900

    콜라츠 수열 만들기

---
+++ pro/lv0/CollatzSequence.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CollatzSequence {
+    public static int[] solution(int n){
+        List<Integer> answer = new ArrayList<>();
+        answer.add(n);
+        while(n > 1){
+            if(n%2 == 0){
+                n /= 2;
+            } else{
+                n = 3 * n + 1;
+            }
+            answer.add(n);
+        }
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(10)));
+    }
+}
