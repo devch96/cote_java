734f592f46d2bd50b7b335fa5fd8afc9e94c96bd
devch96 <chjung1006@gmail.com>
2023-06-20 19:45:43 +0900

    수 조작하기 2

---
+++ pro/lv0/ManipulateNum_2.java
@@ -0,0 +1,16 @@
+public class ManipulateNum_2 {
+    public static String solution(int[] numLog){
+        StringBuilder answer = new StringBuilder();
+        for(int i = 1; i < numLog.length; i++){
+            int interval = numLog[i] - numLog[i-1];
+            if(interval == 1){
+                answer.append("w");
+            } else if (interval == -1) {
+                answer.append("s");
+            } else if (interval == 10) {
+                answer.append("d");
+            } else answer.append("a");
+        }
+        return answer.toString();
+    }
+}
