c3bd53634ccff4321bd002d3eb26ec32b33f44cc
devch96 <chjung1006@gmail.com>
2023-06-20 19:00:38 +0900

    주사위 게임 2

---
+++ pro/lv0/DiceGame_2.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class DiceGame_2 {
+    public int solution(int a, int b, int c){
+        Set<Integer> intSet =new HashSet<>();
+        intSet.add(a);
+        intSet.add(b);
+        intSet.add(c);
+        if(intSet.size() == 1){
+            return (a+b+c)*(a*a + b*b + c*c)*(a*a*a + b*b*b + c*c*c);
+        } else if (intSet.size() ==2) {
+            return (a+b+c) * (a*a + b*b +c*c);
+        } else{
+            return a+b+c;
+        }
+    }
+}
