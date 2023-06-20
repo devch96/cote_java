9ade1420d1fb63b9a3a89a41dff48cc01ba875ee
devch96 <chjung1006@gmail.com>
2023-06-20 18:56:34 +0900

    간단한 식 계산하기

---
+++ pro/lv0/Eval.java
@@ -0,0 +1,14 @@
+public class Eval {
+    public static int solution(String binomial){
+        String[] strArr = binomial.split("\\s+");
+        int a = Integer.parseInt(strArr[0]);
+        int b = Integer.parseInt(strArr[2]);
+        if (strArr[1].equals("+")) {
+            return a+b;
+        } else if(strArr[1].equals("-")){
+            return a-b;
+        } else{
+            return a*b;
+        }
+    }
+}
