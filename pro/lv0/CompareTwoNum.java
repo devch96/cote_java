14ae42abfe498014037b8dceb14b9f931a244e83
devch96 <chjung1006@gmail.com>
2023-06-20 19:28:49 +0900

    두 수의 연산값 비교하기

---
+++ pro/lv0/CompareTwoNum.java
@@ -0,0 +1,7 @@
+public class CompareTwoNum {
+    public static int solution(int a, int b){
+        String s1 = String.valueOf(a);
+        String s2 = String.valueOf(b);
+        return Math.max(Integer.parseInt(s1+s2),2*a*b);
+    }
+}
