f1faa3702cd1e740e8e79b15bc83f0c30e650793
devch96 <chjung1006@gmail.com>
2023-06-21 20:27:08 +0900

    등차수열의 특정한 항만 더하기

---
+++ pro/lv0/SpecificIdxReduce.java
@@ -0,0 +1,13 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class SpecificIdxReduce {
+    public static int solution(int a, int b, boolean[] included){
+        return IntStream.range(0,included.length).filter(i -> included[i] == true).reduce(0,(acc, operand) -> acc + (a+(operand*b)));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(3,4, new boolean[] {true, false, false, true, true}));
+    }
+}
