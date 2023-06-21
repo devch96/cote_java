ba30427629ccdfb0a420cc54a200a5c685d1dbf3
devch96 <chjung1006@gmail.com>
2023-06-21 20:35:10 +0900

    배열 만들기 5

--- pro/lv0/MakeArray_5.java
+++ pro/lv0/MakeArray_5.java
@@ -3,8 +3,4 @@ import java.util.Arrays;
 public class MakeArray_5 {
     public static int[] solution(String[] intStrs, int k, int s, int l){
         return Arrays.stream(intStrs).filter(i -> Integer.parseInt(i.substring(s,s+l))>k).mapToInt(i -> Integer.parseInt(i.substring(s,s+l))).toArray();    }
-
-    public static void main(String[] args) {
-        System.out.println(solution());
-    }
 }
