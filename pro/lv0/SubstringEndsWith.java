7009788804a9f7b166adb81e3cd5594c3dc57c86
devch96 <chjung1006@gmail.com>
2023-06-25 12:33:44 +0900

    특정 문자열로 끝나는 가장 긴 부분 문자열 찾기

---
+++ pro/lv0/SubstringEndsWith.java
@@ -0,0 +1,16 @@
+import java.util.stream.IntStream;
+
+public class SubstringEndsWith {
+    public static String solution(String myString, String pat){
+        return myString.substring(0,IntStream.rangeClosed(0, myString.length())
+                .filter(i -> myString.substring(0, i).endsWith(pat))
+                .reduce((a,b) -> b).getAsInt());
+
+//        return myString.substring(0, myString.lastIndexOf(pat) + pat.length());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("AbCdEFG","dE"));
+
+    }
+}
