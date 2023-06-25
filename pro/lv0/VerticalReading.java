29d28a98701d0c6c44ee8b52833c29b126915f1b
devch96 <chjung1006@gmail.com>
2023-06-25 12:24:20 +0900

    세로 읽기

---
+++ pro/lv0/VerticalReading.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class VerticalReading {
+    public static String solution(String my_string, int m, int c){
+        return Arrays.stream(my_string.split(String.format("(?<=\\G.{%d})",m))).map(i -> String.valueOf(i.charAt(c-1))).collect(Collectors.joining());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("ihrhbakrfpndopljhygc",4,2));
+    }
+}
