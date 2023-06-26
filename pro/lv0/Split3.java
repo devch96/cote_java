c443e1542b4dad4c2e9a73eefc5b457a45eeb485
devch96 <chjung1006@gmail.com>
2023-06-26 18:15:07 +0900

    세 개의 구분자

---
+++ pro/lv0/Split3.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Split3 {
+    public String[] solution(String myStr){
+        return myStr.split("[abc]").length > 0 ?
+                Arrays.stream(myStr.split("[abc]")).filter(i -> i.length() != 0).toArray(String[]::new) :
+                new String[] { "EMPTY" };
+    }
+}
