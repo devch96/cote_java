07b859c4d99ebb017169ae675be14dc959e511e3
devch96 <chjung1006@gmail.com>
2023-06-26 17:32:01 +0900

    문자열이 몇 번 등장하는지 세기

---
+++ pro/lv0/CountStringPat.java
@@ -0,0 +1,16 @@
+public class CountStringPat {
+    public static int solution(String myString, String pat){
+        int answer = 0;
+        for(int i = 0; i < myString.length(); i++){
+            if(myString.substring(i).startsWith(pat)){
+                answer ++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("banana","ana"));
+        System.out.println(solution("aaaa","aa"));
+    }
+}
