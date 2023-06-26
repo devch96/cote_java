90825d6b6a1791ea70ec9f693b3452032dfef7cb
devch96 <chjung1006@gmail.com>
2023-06-26 18:19:36 +0900

    수열과 구간 쿼리 4

---
+++ pro/lv0/SequenceAndIntervalQueries_4.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class SequenceAndIntervalQueries_4 {
+    public static int[] solution(int[] arr, int[][] queries){
+        Arrays.stream(queries).forEach(query -> {
+            for(int i = query[0]; i <= query[1] ;i++){
+                if(i%query[2] == 0){
+                    arr[i] += 1;
+                }
+            }
+        });
+        return arr;
+    }
+}
