4768bdada42c81216ba434d71f0130ebe261ce96
devch96 <chjung1006@gmail.com>
2023-06-26 18:30:37 +0900

    리스트 자르기

---
+++ pro/lv0/SlicingArray.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class SlicingArray {
+    public int[] solution(int n, int[] slicer, int[] num_list){
+        int a = slicer[0];
+        int b = slicer[1];
+        int c = slicer[2];
+        if(n == 1) {
+            return Arrays.copyOfRange(num_list, 0, b+1);
+        } else if (n == 2) {
+            return Arrays.copyOfRange(num_list,a,num_list.length);
+
+        } else if (n == 3) {
+            return Arrays.copyOfRange(num_list,a,b+1);
+
+        }else {
+            int[] answer = new int[((b-a)/c)+1];
+            int index = 0;
+            for(int i = a; i <= b ; i+=c){
+                answer[index] = num_list[i];
+                index++;
+            }
+            return answer;
+        }
+    }
+}
