d2a3e38ba6b6fbd68d88c605661a86d299399042
devch96 <chjung1006@gmail.com>
2023-06-27 18:13:01 +0900

    두 수의 합

---
+++ pro/lv0/APlusB.java
@@ -0,0 +1,19 @@
+import java.math.BigInteger;
+
+public class APlusB {
+    public String solution(String a, String b){
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        for(int i =0; i < Math.max(a.length(),b.length()); i++){
+            int numA = i < a.length() ? a.charAt(a.length() -1 - i) - '0' : 0;
+            int numB = i < b.length() ? b.charAt(b.length() -1 - i) - '0' : 0;
+            int sum = numA + numB + carry;
+            carry = sum/10;
+            sb.insert(0, sum % 10);
+        }
+        if(carry > 0){
+            sb.insert(0, carry);
+        }
+        return sb.toString();
+    }
+}
