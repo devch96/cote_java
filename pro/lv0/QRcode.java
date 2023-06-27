5f2a2598f85c9a72005f7fd010a0bae40f387cc8
devch96 <chjung1006@gmail.com>
2023-06-27 17:19:14 +0900

    qr code

---
+++ pro/lv0/QRcode.java
@@ -0,0 +1,9 @@
+public class QRcode {
+    public static String solution(int q, int r, String code){
+        StringBuilder answer = new StringBuilder();
+        for(int i = r; i < code.length(); i+=q){
+            answer.append(code.charAt(i));
+        }
+        return answer.toString();
+    }
+}
