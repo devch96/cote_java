717edbc0ead981cfe15bf6fbcd9b98fd002909c6
devch96 <chjung1006@gmail.com>
2023-06-27 17:16:05 +0900

    문자열 묶기

---
+++ pro/lv0/BindString.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class BindString {
+    public static int solution(String[] strArr){
+        int[] lenArr = Arrays.stream(strArr).mapToInt(i->i.length()).toArray();
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for(int len : lenArr){
+            frequencyMap.put(len, frequencyMap.getOrDefault(len,0) + 1);
+        }
+        int maxFrequency = 0;
+        for (Map.Entry<Integer,Integer> entry : frequencyMap.entrySet()){
+            int frequency = entry.getValue();
+            if(frequency > maxFrequency){
+                maxFrequency = frequency;
+            }
+        }
+        return maxFrequency;
+    }
+}
