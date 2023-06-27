97f71dde6282a53c0254f6d3a83ba272daaa3c1a
devch96 <chjung1006@gmail.com>
2023-06-27 17:49:17 +0900

    2의 영역

---
+++ pro/lv0/AreaOf2.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class AreaOf2 {
+    public static int[] solution(int[] arr){
+        int[] arr2 = IntStream.range(0, arr.length).filter(i -> arr[i] == 2).toArray();
+        return arr2.length != 0 ? Arrays.copyOfRange(arr, arr2[0], arr2[arr2.length-1]+1) : new int[] {-1};
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[] {1, 2, 1, 4, 5, 2, 9})));
+    }
+}
