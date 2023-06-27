ceb8771d113e6e6de37d2609b806d19bea2849ad
devch96 <chjung1006@gmail.com>
2023-06-27 18:34:39 +0900

    수열과 구간 쿼리 2

---
+++ pro/lv0/SequenceAndIntervalQueries_2.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class SequenceAndIntervalQueries_2 {
+    public static int[] solution(int[] arr, int[][] queries){
+        return Arrays.stream(queries).mapToInt(query -> {
+            int s = query[0];
+            int e = query[1];
+            int k = query[2];
+            return IntStream.rangeClosed(s,e).filter(i -> arr[i] > k).map(i -> arr[i]).min().orElse(-1);
+        }).toArray();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[] {0,1,2,4,3}, new int[][] {{0, 4, 2},{0, 3, 2},{0, 2, 2}})));
+    }
+}
