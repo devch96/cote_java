93d0cd0c7fe4d3ea15c1950c3445fd053f6f8b9f
devch96 <chjung1006@gmail.com>
2023-06-28 18:18:23 +0900

    문자열 겹쳐쓰기

---
+++ pro/lv0/OverwriteString.java
@@ -0,0 +1,6 @@
+public class OverwriteString {
+    public String solution(String my_string, String overwrite_string, int s){
+        StringBuilder sb = new StringBuilder(my_string);
+        return sb.replace(s,overwrite_string.length()+s, overwrite_string).toString();
+    }
+}
