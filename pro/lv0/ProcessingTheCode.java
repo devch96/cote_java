7391a19e72985b3bbb53209ef48820e482dcfe8c
devch96 <chjung1006@gmail.com>
2023-06-29 17:58:05 +0900

    코드 처리하기

---
+++ pro/lv0/ProcessingTheCode.java
@@ -0,0 +1,26 @@
+public class ProcessingTheCode {
+    public String solution(String code) {
+        StringBuilder ret = new StringBuilder();
+        int mode = 0;
+        for(int idx = 0; idx < code.length(); idx++){
+            if(mode == 0){
+                if(code.charAt(idx) == '1'){
+                    mode = 1;
+                }else{
+                    if(idx%2 == 0){
+                        ret.append(code.charAt(idx));
+                    }
+                }
+            }else{
+                if(code.charAt(idx) == '1'){
+                    mode = 0;
+                }else{
+                    if(idx%2 != 0){
+                        ret.append(code.charAt(idx));
+                    }
+                }
+            }
+        }
+        return ret.length() > 0 ? ret.toString() : "EMPTY";
+    }
+}
